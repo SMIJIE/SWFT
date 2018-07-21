a702576709011968fafc9ee0389c4ba9321d3482
Zakusylo <ZakusyloP@gmail.com>
2018-07-21 19:53:41 +0300

    commit: Write javaDoc for classes 'Converter'

--- src/main/java/ua/training/model/dao/utility/converter/FoodCategoryConverter.java
+++ src/main/java/ua/training/model/dao/utility/converter/FoodCategoryConverter.java
@@ -5,14 +5,26 @@ import ua.training.model.entity.enums.FoodCategory;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
+/**
+ * Description: Convert ua.training.model.entity.enums.FoodCategory to java.lang.String and back
+ *
+ * @author Zakusylo Pavlo
+ */
 @Converter(autoApply = true)
 public class FoodCategoryConverter implements AttributeConverter<FoodCategory, String> {
-
+    /**
+     * @param attribute FoodCategory
+     * @return object String
+     */
     @Override
     public String convertToDatabaseColumn(FoodCategory attribute) {
         return attribute.getValue();
     }
 
+    /**
+     * @param dbData String
+     * @return object FoodCategory
+     */
     @Override
     public FoodCategory convertToEntityAttribute(String dbData) {
         return FoodCategory.valueOf(dbData);
--- src/main/java/ua/training/model/dao/utility/converter/FoodIntakeConverter.java
+++ src/main/java/ua/training/model/dao/utility/converter/FoodIntakeConverter.java
@@ -5,14 +5,26 @@ import ua.training.model.entity.enums.FoodIntake;
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
+/**
+ * Description: Convert ua.training.model.entity.enums.FoodIntake to java.lang.String and back
+ *
+ * @author Zakusylo Pavlo
+ */
 @Converter(autoApply = true)
 public class FoodIntakeConverter implements AttributeConverter<FoodIntake, String> {
-
+    /**
+     * @param attribute FoodIntake
+     * @return object String
+     */
     @Override
     public String convertToDatabaseColumn(FoodIntake attribute) {
         return attribute.getValue();
     }
 
+    /**
+     * @param dbData String
+     * @return object FoodIntake
+     */
     @Override
     public FoodIntake convertToEntityAttribute(String dbData) {
         return FoodIntake.valueOf(dbData);
