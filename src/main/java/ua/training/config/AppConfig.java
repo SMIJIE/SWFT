46d194d60d86146e2611601fc44968ac612693ff
Zakusylo <ZakusyloP@gmail.com>
2018-10-06 23:07:51 +0300

    commit: Add application config

---
+++ src/main/java/ua/training/config/AppConfig.java
@@ -0,0 +1,90 @@
+package ua.training.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import java.util.Locale;
+
+/**
+ * Description: This is the AppConfiguration class
+ *
+ * @author Zakusylo Pavlo
+ */
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@PropertySource("classpath:db.properties")
+@ComponentScan({"ua.training"})
+public class AppConfig implements WebMvcConfigurer {
+
+    /**
+     * Register static resources such as *.ccs,*.js etc.
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/**")
+                .addResourceLocations("/WEB-INF/resources/");
+    }
+
+    /**
+     * Register (i.e. Servlets and JSPs)
+     */
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
+
+    /**
+     * Register resource bundle message source
+     */
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        messageSource.setUseCodeAsDefaultMessage(true);
+
+        return messageSource;
+    }
+
+    /**
+     * Set default locale
+     */
+    @Bean
+    public SessionLocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("uk", "UA"));
+
+        return localeResolver;
+    }
+
+    /**
+     * Add locale change interceptor
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+
+        registry.addInterceptor(localeChangeInterceptor);
+    }
+}
